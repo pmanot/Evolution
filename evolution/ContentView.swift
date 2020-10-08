709bb3ae42f62561b81f685c3ed04bf388b07c0b
Purav <manot.purav@gmail.com>
2020-10-09 02:46:15 +0530

    Initial Commit

---
+++ evolution/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  evolution
+//
+//  Created by Purav Manot on 09/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ evolution/evolutionApp.swift
@@ -0,0 +1,17 @@
+//
+//  evolutionApp.swift
+//  evolution
+//
+//  Created by Purav Manot on 09/10/20.
+//
+
+import SwiftUI
+
+@main
+struct evolutionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
