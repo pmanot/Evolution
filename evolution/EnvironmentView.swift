f0d0ed3c2320ac23cc40345fa19d6bbdcbddc792
Purav <manot.purav@gmail.com>
2020-11-07 04:27:15 +0530

    Rename to SpeciesEnvironment

--- evolution/EnvironmentView.swift
+++ evolution/EnvironmentView.swift
@@ -8,25 +8,19 @@
 import SwiftUI
 
 struct EnvironmentView: View {
-    @EnvironmentObject var env: Environment
+    @EnvironmentObject var env: SpeciesEnvironment
     var body: some View {
         GeometryReader { frame in
             ZStack {
-                RoundedRectangle(cornerRadius: 10)
-                    .overlay(RoundedRectangle(cornerRadius: 8).foregroundColor(.black))
                 FoodView()
             }
-            .onAppear{
-                env.bounds = Bounds(left: 20, right: frame.size.width - 20, up: 20, down: frame.size.height - 20)
-            }
         }
-        
     }
 }
 
 struct EnvironmentView_Previews: PreviewProvider {
     static var previews: some View {
         EnvironmentView()
-            .environmentObject(Environment())
+            .environmentObject(SpeciesEnvironment())
     }
 }
