e2880ed179de01452be862e6ae4df19663dc894e
Purav <manot.purav@gmail.com>
2020-11-07 04:31:01 +0530

    New design
    Create a more detailed and informative cell redesign

--- evolution/Cell.swift
+++ evolution/Cell.swift
@@ -10,12 +10,10 @@ import SwiftUI
 struct Cell: View {
     var some_species: Species
     var style: CellStyle = .circle
-    var color: Color
-    @EnvironmentObject var env: Environment
+    @EnvironmentObject var env: SpeciesEnvironment
     init(_ species: Species, style: CellStyle = .circle) {
         some_species = species
         self.style = style
-        color = species.color
     }
     var body: some View {
         switch style {
@@ -23,35 +21,25 @@ struct Cell: View {
         case .circle:
             ZStack {
                 Circle()
-                    .opacity(0.4)
-                    .overlay(Circle().stroke(lineWidth: 2).foregroundColor(.black).opacity(0.8))
-                    .frame(width: 15, height: 15)
-                    .foregroundColor(color)
-                    .position(some_species.coordinates.cg())
-                if some_species.foodEaten { // if food eaten, display a small dot like indicator within view to show
-                    Circle()
-                        .opacity(0.4)
-                        .overlay(Rectangle().stroke(lineWidth: 2).foregroundColor(.black).opacity(0.8))
-                        .frame(width: 4, height: 4)
-                        .position(some_species.coordinates.cg())
-                }
+                    .strokeBorder(Color.darkJungleGreen)
+                    .frame(width: 25, height: 25)
+                    .foregroundColor(.blueGreen)
+                Circle()
+                    .strokeBorder(Color.blueGreen, lineWidth: 6, antialiased: true)
+                    .overlay(Arc(startAngle: .degrees(0), endAngle: angle(some_species.lifespan, max: some_species.maxLifespan), clockwise: true).stroke(lineWidth: 2).frame(width: 25, height: 25).foregroundColor(.black))
+                    .overlay(some_species.foodEnergy.count >= 1 ? Color.green.opacity(0.2) : Color.lightBlueGray.opacity(0.2)).clipShape(Circle())
+                    .frame(width: 25, height: 25)
+                Circle()
+                    .foregroundColor(.darkJungleGreen)
+                    .frame(width: 2, height: 2)
             }
+            .overlay(Color.red.opacity(some_species.disabled ? 0.6 : 0).clipShape(Circle()))
+            .position(some_species.coordinates.cg())
+            .animation(.easeInOut(duration: 0.5))
         
         case .square:
             ZStack {
-                Rectangle()
-                    .opacity(0.4)
-                    .overlay(Rectangle().stroke(lineWidth: 2).foregroundColor(.black).opacity(0.8))
-                    .frame(width: 15, height: 15)
-                    .foregroundColor(color)
-                    .position(some_species.coordinates.cg())
-                if some_species.foodEaten { // if food eaten, display a small dot like indicator within view to show
-                    Rectangle()
-                        .opacity(0.4)
-                        .overlay(Rectangle().stroke(lineWidth: 2).foregroundColor(.black).opacity(0.8))
-                        .frame(width: 4, height: 4)
-                        .position(some_species.coordinates.cg())
-                }
+                // in progress
             }
         }
     }
@@ -64,8 +52,30 @@ enum CellStyle { // two styles for visual representation of a species
 
 struct Cell_Previews: PreviewProvider {
     static var previews: some View {
-        Cell(gen(coordinates: Point(x: 200, y: 400), Environment().bounds))
-            .environmentObject(Environment())
+        Cell(gen(coordinates: Point(x: 200, y: 400), SpeciesEnvironment().bounds))
+            .environmentObject(SpeciesEnvironment())
     }
 }
+
+func angle(_ val: Int, max: Int) -> Angle {
+    return Angle.degrees(val.mappedValue(inputRange: 0..<max, outputRange: 0..<360))
+}
+
+struct Arc: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    var clockwise: Bool
+
+    func path(in rect: CGRect) -> Path {
+        let rotationAdjustment = Angle.degrees(90)
+        let modifiedStart = startAngle - rotationAdjustment
+        let modifiedEnd = endAngle - rotationAdjustment
+
+        var path = Path()
+        path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+        return path
+    }
+}
+
 
