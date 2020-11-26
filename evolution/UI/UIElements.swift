c7551dd4a951ebaa84a3e1599cdad5ef0ba61a43
Purav <manot.purav@gmail.com>
2020-11-26 22:42:50 +0530

    Create FloatingButtonStyle()

--- evolution/UI/UIElements.swift
+++ evolution/UI/UIElements.swift
@@ -11,9 +11,14 @@ struct UIElements: View {
     var body: some View {
         VStack {
             Button(action: {}) {
-                
+                ZStack {
+                    Text("Test")
+                    Circle()
+                        .stroke(lineWidth: 4)
+                        .frame(width: 70, height: 70)
+                }
             }
-            .buttonStyle(FunctionalButton())
+            .buttonStyle(FloatingButtonStyle())
         }
     }
 }
@@ -24,7 +29,7 @@ struct UIElements_Previews: PreviewProvider {
     }
 }
 
-struct FunctionalButton: ButtonStyle {
+struct FunctionalButtonStyle: ButtonStyle {
     var cornerRadius: CGFloat
     init(_ corner: CGFloat = 8) {
         cornerRadius = corner
@@ -42,6 +47,19 @@ struct FunctionalButton: ButtonStyle {
     }
 }
 
+struct FloatingButtonStyle: ButtonStyle {
+    var colors: [Color]
+    init(_ defaultColor: Color = .black, _ pressedColor: Color = .gray) {
+        colors = [defaultColor, pressedColor]
+    }
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .shadow(radius: configuration.isPressed ? 1 : 2, x: !configuration.isPressed ? 6.4 : 0.8, y: !configuration.isPressed ? 8 : 1)
+            .offset(x: configuration.isPressed ? 6.4 : 0.6, y: configuration.isPressed ? 8 : 1)
+            .animation(.easeOut(duration: 0.5))
+            .foregroundColor(configuration.isPressed ? colors[1] : colors[0])
+    }
+}
 
 struct Bright: ViewModifier {
     var amount: Double
