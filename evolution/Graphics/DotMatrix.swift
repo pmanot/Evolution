4c311ea09fb4cb8767e10413ed38acb968fa3a13
Purav <manot.purav@gmail.com>
2020-12-09 04:35:15 +0530

    Replace Circle() with static image of circle

--- evolution/Graphics/DotMatrix.swift
+++ evolution/Graphics/DotMatrix.swift
@@ -10,23 +10,103 @@ import SwiftUI
 struct DotMatrix: View {
     var height: Int
     var width: Int
-    init(_ width: Int = 300, _ height: Int =  500) {
-        self.height = height/20
-        self.width = width/20
+    var circleSize: CGFloat = 1.5
+    init(_ circleSize: CGFloat = 1.5, _ width: Int = 300, _ height: Int =  500) {
+        self.height = height/80
+        self.width = width/80
+        self.circleSize = circleSize
     }
     var body: some View {
         VStack(spacing: 0) {
             ForEach(0..<height, id: \.self){ n in
                 HStack(spacing: 0) {
                     ForEach(0..<width, id: \.self){ n in
-                        Circle()
-                            .frame(width: 1.5, height: 1.5)
-                            .frame(width: 30, height: 30)
+                        HStack(spacing: 0) {
+                            VStack(spacing: 0) {
+                                Group {
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                }
+                            }
+                            VStack(spacing: 0) {
+                                Group {
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                }
+                            }
+                            VStack(spacing: 0) {
+                                Group {
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                }
+                            }
+                            VStack(spacing: 0) {
+                                Group {
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                    Image(systemName: "circle").resizable()
+                                        .antialiased(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                                        .frame(width: circleSize, height: circleSize)
+                                        .frame(width: 30, height: 30)
+                                }
+                            }
+                        }
                     }
                 }
             }
         }
-        .drawingGroup()
     }
 }
 
